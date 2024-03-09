b2f35bd968264ffa46b8adf5cdd2c21fd899c9d4
Nicolle Policiano <nicolle.fabri@gmail.com>
2024-03-08 21:18:22 -0600

    IOS-83: Connect the view with the code

--- Pokemon/PokemonViewController.swift
+++ Pokemon/PokemonViewController.swift
@@ -7,14 +7,44 @@
 
 import UIKit
 
-class PokemonViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Apareceu")
-    }
-
-
+struct Pokemon {
+    let name: String
+    let primaryAttribute: String
+    let secondaryAttribute: String?
+    let specie: String
+    let image: UIImage
+    let predominantColor: UIColor
 }
 
+class PokemonViewController: UIViewController {
+    let pokemon = Pokemon(
+        name: "Pikachu",
+        primaryAttribute: "Electric",
+        secondaryAttribute: nil,
+        specie: "Mouse",
+        image: .pikachu,
+        predominantColor: UIColor(named: "Yellow")!
+    )
+
+    @IBOutlet weak var specieLabel: UILabel!
+    @IBOutlet weak var primaryAttributeLabel: UILabel!
+    @IBOutlet weak var secondaryAttributeView: UIView!
+    @IBOutlet weak var secondaryAttributeLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        title = pokemon.name
+        view.backgroundColor = pokemon.predominantColor
+
+        primaryAttributeLabel.text = pokemon.primaryAttribute
+        specieLabel.text = "\(pokemon.specie) Pokemon"
+        imageView.image = pokemon.image
+
+        if let secondaryAttribute = pokemon.secondaryAttribute {
+            secondaryAttributeLabel.text = secondaryAttribute
+            secondaryAttributeView.isHidden = false
+        } else {
+            secondaryAttributeView.isHidden = true
+        }
+    }
+}
