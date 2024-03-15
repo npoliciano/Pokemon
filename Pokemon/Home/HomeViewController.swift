11ec05886e77d39453678078e40bb14d0e2a7af8
Nicolle Policiano <nicolle.fabri@gmail.com>
2024-03-14 21:48:45 -0500

    IOS-93: Implement PokemonCell

--- Pokemon/Home/HomeViewController.swift
+++ Pokemon/Home/HomeViewController.swift
@@ -23,20 +23,18 @@ final class HomeViewController: UIViewController, UICollectionViewDataSource, UI
     var items: [PokemonCell.Pokemon] = [
         .init(
             name: "Pikachu",
-            imageUrl: String("https://firebasestorage.googleapis.com/v0/b/pokedex-bb36f.appspot.com/o/pokemon_images%2FAE45F260-9012-45F5-BE75-5C8972014EAA?alt=media&token=55b2ea2f-d525-47c0-95ae-7ddf01e879d3")
+            imageUrl: URL(string: "https://firebasestorage.googleapis.com/v0/b/pokedex-bb36f.appspot.com/o/pokemon_images%2F62CDB87E-9A76-4184-B82A-4FB38CDA2BD0?alt=media&token=82726149-f9a6-4299-8837-6bae1ecb1081")!,
+            primaryAttribute: "Grass",
+            secondaryAttribute: "Poison",
+            backgroundColor: .pikachu
         ),
         .init(
             name: "Pikachu",
-            imageUrl: String("https://firebasestorage.googleapis.com/v0/b/pokedex-bb36f.appspot.com/o/pokemon_images%2FAE45F260-9012-45F5-BE75-5C8972014EAA?alt=media&token=55b2ea2f-d525-47c0-95ae-7ddf01e879d3")
-        ),
-        .init(
-            name: "Pikachu",
-            imageUrl: String("https://firebasestorage.googleapis.com/v0/b/pokedex-bb36f.appspot.com/o/pokemon_images%2FAE45F260-9012-45F5-BE75-5C8972014EAA?alt=media&token=55b2ea2f-d525-47c0-95ae-7ddf01e879d3")
-        ),
-        .init(
-            name: "Pikachu",
-            imageUrl: String("https://firebasestorage.googleapis.com/v0/b/pokedex-bb36f.appspot.com/o/pokemon_images%2FAE45F260-9012-45F5-BE75-5C8972014EAA?alt=media&token=55b2ea2f-d525-47c0-95ae-7ddf01e879d3")
-        ),
+            imageUrl: URL(string: "https://firebasestorage.googleapis.com/v0/b/pokedex-bb36f.appspot.com/o/pokemon_images%2FAE45F260-9012-45F5-BE75-5C8972014EAA?alt=media&token=55b2ea2f-d525-47c0-95ae-7ddf01e879d3")!,
+            primaryAttribute: "Electric",
+            secondaryAttribute: nil,
+            backgroundColor: .pikachu
+        )
     ]
 
     override func viewDidLoad() {
@@ -73,10 +71,7 @@ final class HomeViewController: UIViewController, UICollectionViewDataSource, UI
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PokemonCell.identifier, for: indexPath) as! PokemonCell
 
         let pokemon = items[indexPath.row]
-        cell.nameLabel.text = pokemon.name
-        cell.imageView.image = UIImage(named: pokemon.imageUrl)
-
-        cell.backgroundColor = UIColor.cyan
+        cell.setContent(with: pokemon)
         return cell
     }
 
@@ -93,7 +88,7 @@ final class HomeViewController: UIViewController, UICollectionViewDataSource, UI
         let availableWidth = view.frame.width - paddingSpace
         let widthPerItem = availableWidth / itemsPerRow
 
-        return CGSize(width: widthPerItem, height: widthPerItem)
+        return CGSize(width: widthPerItem, height: widthPerItem * 0.8)
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
