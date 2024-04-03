29b39189ab9e105f4d7aa328a32a2d35dd0897ee
Nicolle Policiano <nicolle.fabri@gmail.com>
2024-04-02 22:28:44 -0500

    IOS-117: Ensure test fails on failure to get Pokemons

--- PokemonTests/HomeAPITests.swift
+++ PokemonTests/HomeAPITests.swift
@@ -17,13 +17,37 @@ final class HomeAPITests: XCTestCase {
 
         XCTAssertEqual(service.getDataCalls, 0)
     }
+
+    func testFailsToGetPokemons() {
+        let service = PokemonsDataSourceSpy()
+        let sut = HomeAPI(database: service)
+        trackForMemoryLeaks(sut, service)
+        service.expectedResult = .failure(ErrorDummy())
+
+        var actualError: Error?
+        let subscription = sut.getPokemons()
+            .sink(
+                receiveCompletion: { completion in
+                    if case.failure(let error) = completion {
+                        actualError = error
+                    }
+                },
+                receiveValue: { _ in }
+            )
+
+        XCTAssertEqual(actualError as? ErrorDummy, ErrorDummy())
+    }
 }
 
 final class PokemonsDataSourceSpy: PokemonsDataSource {
     private(set) var getDataCalls = 0
 
+    var expectedResult = Result<[PokemonJSON], Error>.failure(ErrorDummy())
+
     func getData() -> AnyPublisher<[PokemonJSON], Error> {
         getDataCalls += 1
-        fatalError()
+        return expectedResult
+            .publisher
+            .eraseToAnyPublisher()
     }
 }
